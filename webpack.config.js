047b41d027aec95760443bbc52a27c88693eff28
Leif Gensert <leif@leif.io>
2016-03-05 17:10:47 +0100

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -9,22 +9,32 @@ const PATHS = {
   build: path.join(__dirname, "build")
 }
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
+  entry: {
+    app: PATHS.app,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: "bundle.js",
+  },
   module: {
     loaders: [
       {
         test: /\.css$/,
         loaders: ["style", "css"],
         include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
       }
     ]
-  },
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: "bundle.js",
   }
 }
 
@@ -34,9 +44,6 @@ if(TARGET === "start" || !TARGET) {
     devServer: {
       contentBase: PATHS.build,
 
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
       historyApiFallback: true,
       hot: true,
       inline: true,
@@ -45,13 +52,6 @@ if(TARGET === "start" || !TARGET) {
       // Display only errors to reduce the amount of output.
       stats: "errors-only",
 
-      // Parse host and port from env so this is easy to customize.
-      //
-      // If you use Vagrant or Cloud9, set
-      // host: process.env.HOST || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices unlike default
-      // localhost
       host: process.env.HOST,
       port: process.env.PORT
     },
