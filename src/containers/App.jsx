31d0eef651c18f2ea3338b3b2dc8c463064af25f
Leif Gensert <leif@leif.io>
2016-03-09 01:21:55 +0100

    add test for rendering app

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,19 @@
 import FormulonApp from './FormulonApp';
 import React from 'react';
 
+import {Provider} from 'react-redux';
+import configureStore from '../configureStore'
+
+const store = configureStore();
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Formulon</h1>
-        <FormulonApp />
+        <Provider store={store}>
+          <FormulonApp />
+        </Provider>
       </div>
     );
   }
