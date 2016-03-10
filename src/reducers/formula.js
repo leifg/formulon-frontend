a0de2fcd20ec1a4ae4db8cf412db2a7fd9bfbed5
Leif Gensert <leif@leif.io>
2016-03-11 00:13:20 +0100

    first link to formulon (awful)

--- src/reducers/formula.js
+++ src/reducers/formula.js
@@ -1,6 +1,5 @@
 import { CHANGE_FORMULA } from '../constants/ActionTypes'
-import map from 'lodash/map'
-import includes from 'lodash/includes'
+import Formulon from "../../../formulon/src/formulon"
 
 const initialState = {
   formula: null,
@@ -10,9 +9,12 @@ const initialState = {
 export default function formula(state = initialState, action) {
   switch (action.type) {
     case CHANGE_FORMULA:
+      let identifiers = Formulon.extract(action.formula).reduce((agg, id)  => {
+        return Object.assign({}, agg, {[id]: {}})
+      }, state.identifiers)
       return {
         formula: action.formula,
-        identifiers: []
+        identifiers: identifiers
       }
     default:
       return state
