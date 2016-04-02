6e235401467f920451f943a1a699a5608f9ab5ed
Leif Gensert <leif@leif.io>
2016-04-02 14:40:15 +0200

    implement transformation layer
    closes #10

--- src/utils/salesforceUtils.js
+++ src/utils/salesforceUtils.js
@@ -14,3 +14,31 @@ export const defaultMeta = (type) => {
       return {}
   }
 }
+
+export const transformIdentifiers = (identifersList = []) => {
+  return identifersList.reduce((agg, identifier) => {
+    return Object.assign(
+      agg,
+      {
+        [identifier.name]: {
+          value: coerceData(identifier.value, identifier.dataType),
+          dataType: identifier.dataType,
+          meta: defaultMeta(identifier.dataType)
+        }
+      }
+    )
+  }, {})
+}
+
+// private
+
+const coerceData = (value, dataType) => {
+  switch (dataType) {
+    case 'number':
+      return parseInt(value, 10)
+    case 'checkbox':
+      return value === true
+    default:
+      return value
+  }
+}
--- test/utils/salesforceUtils.spec.js
+++ test/utils/salesforceUtils.spec.js
@@ -1,6 +1,6 @@
-/* global describe it */
+/* global context describe it */
 
-import { availableDataTypes, defaultMeta } from '../../src/utils/salesforceUtils'
+import { availableDataTypes, defaultMeta, transformIdentifiers } from '../../src/utils/salesforceUtils'
 
 import chai from 'chai'
 const expect = chai.expect
@@ -28,5 +28,86 @@ describe('defaultMeta', () => {
 
   it('returns empty hash for unknown value', () => {
     expect(defaultMeta('weirdotype')).to.deep.eq({})
+  })
+})
+
+describe('transformIdentifiers', () => {
+  context('empty input', () => {
+    it('returns empty object for empty array', () => {
+      expect(transformIdentifiers([])).to.deep.eq({})
+    })
+  })
+
+  context('undefined input', () => {
+    it('returns empty object for empty array', () => {
+      expect(transformIdentifiers(undefined)).to.deep.eq({})
+    })
+  })
+
+  context('undefined checkbox', () => {
+    const input = [
+      {
+        name: 'var1',
+        value: undefined,
+        dataType: 'checkbox'
+      }
+    ]
+
+    const expectedOutput = {
+      var1: {
+        value: false,
+        dataType: 'checkbox',
+        meta: {}
+      }
+    }
+
+    it('returns expected output', () => {
+      expect(transformIdentifiers(input)).to.deep.eq(expectedOutput)
+    })
+  })
+
+  context('list of identifiers', () => {
+    const input = [
+      {
+        name: 'var1',
+        value: '1',
+        dataType: 'number'
+      },
+      {
+        name: 'var2',
+        value: 'content',
+        dataType: 'text'
+      },
+      {
+        name: 'var3',
+        value: true,
+        dataType: 'checkbox'
+      }
+    ]
+
+    const expectedOutput = {
+      var1: {
+        value: 1,
+        dataType: 'number',
+        meta: {
+          length: 8,
+          scale: 0
+        }
+      },
+      var2: {
+        value: 'content',
+        dataType: 'text',
+        meta: {}
+      },
+      var3: {
+        value: true,
+        dataType: 'checkbox',
+        meta: {}
+      }
+    }
+
+    it('returns expected output', () => {
+      expect(transformIdentifiers(input)).to.deep.eq(expectedOutput)
+    })
   })
 })
