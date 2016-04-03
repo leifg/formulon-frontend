300f3fd6af2631b1224f84348ff1ff0745360f96
Leif Gensert <leif@leif.io>
2016-04-03 18:01:44 +0200

    add bootstrap css to project

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import webpack from 'webpack'
 import webpackConfig from './webpack.config.js'
 import WebpackDevServer from 'webpack-dev-server'
 import gutil from 'gulp-util'
+import del from 'del'
+import sass from 'gulp-sass'
 
 gulp.task('default', ['webpack-dev-server'])
 
@@ -18,19 +20,28 @@ gulp.task('webpack', (callback) => {
   })
 })
 
+gulp.task('clean', () => {
+  return del('./public/**/*')
+})
+
 gulp.task('html', () => {
   return gulp.src('./src/index.html').pipe(gulp.dest('./public'))
 })
 
-gulp.task('webpack-dev-server', ['html', 'webpack'], (callback) => {
+gulp.task('styles', () => {
+  return gulp.src('./src/main.scss').pipe(sass()).pipe(gulp.dest('./public'))
+})
+
+gulp.task('webpack-dev-server', ['clean', 'html', 'styles', 'webpack'], (callback) => {
   const compiler = webpack(webpackConfig)
+  const port = '9000'
   const devServerOptions = {
     contentBase: './public',
     host: '0.0.0.0',
-    port: '9000'
+    port: port
   }
 
   new WebpackDevServer(compiler, devServerOptions).listen(9000, 'localhost', (_err) => {
-    console.log('Server started')
+    console.log(`Server started on port ${port}`)
   })
 })
