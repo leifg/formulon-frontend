ffce539eca40a96ca909e8370c9e299408cca3e5
Leif Gensert <leif@leif.io>
2016-04-03 13:57:44 +0200

    add redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,14 @@
-import {combineReducers, createStore, applyMiddleware} from 'redux'
+import {combineReducers, compose, createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import * as reducers from './reducers'
 
 const loggerMiddleware = createLogger()
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,
-  loggerMiddleware
+const createStoreWithMiddleware = compose(
+  applyMiddleware(thunkMiddleware),
+  applyMiddleware(loggerMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
 )(createStore)
 
 export default function configureStore () {
