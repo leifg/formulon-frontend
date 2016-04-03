94e512913b5d1e74637eab02c8f64a0246b77835
Leif Gensert <leif@leif.io>
2016-04-03 17:42:18 +0200

    add gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import gulp from 'gulp'
+import webpack from 'webpack'
+import webpackConfig from './webpack.config.js'
+import WebpackDevServer from 'webpack-dev-server'
+import gutil from 'gulp-util'
+
+gulp.task('default', ['webpack-dev-server'])
+
+gulp.task('webpack', (callback) => {
+  webpack(webpackConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err)
+    gutil.log('[webpack]', stats.toString({
+        // output options
+    }))
+    callback()
+  })
+})
+
+gulp.task('html', () => {
+  return gulp.src('./src/index.html').pipe(gulp.dest('./public'))
+})
+
+gulp.task('webpack-dev-server', ['html', 'webpack'], (callback) => {
+  const compiler = webpack(webpackConfig)
+  const devServerOptions = {
+    contentBase: './public',
+    host: '0.0.0.0',
+    port: '9000'
+  }
+
+  new WebpackDevServer(compiler, devServerOptions).listen(9000, 'localhost', (_err) => {
+    console.log('Server started')
+  })
+})
