50507b236bd941cbc89e02e1ca017f7b506078f1
Leif Gensert <leif@leif.io>
2016-04-10 23:55:16 +0200

    chore: remove devtools from production
    envify build script by passing in variables using the DefinePlugin
    (details:
    https://github.com/webpack/docs/wiki/list-of-plugins#defineplugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'eval',
@@ -12,6 +12,10 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.VERSION': JSON.stringify(require('./package.json')).version,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
     new webpack.NoErrorsPlugin()
   ],
   resolve: {
