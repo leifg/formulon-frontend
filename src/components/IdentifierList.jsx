09784e287f09ecc2e006d578943452fefd788a80
Leif Gensert <leif@leif.io>
2016-04-16 19:31:32 +0200

    refactor: use grid system
    makes it easier to align list of identifiers

--- src/components/IdentifierList.jsx
+++ src/components/IdentifierList.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import { Row } from 'react-lightning-design-system'
+import { Grid } from 'react-lightning-design-system'
 import Identifier from './Identifier'
 
 export default class IdentifierList extends Component {
   render () {
     const { identifiers, changeIdentifierDataType, changeIdentifierOptions, changeIdentifierValue } = this.props
     return (
-      <Row>
+      <Grid>
         {identifiers.map((identifierAttributes) =>
-          <Row>
+          [
+            <h2 className='slds-m-vertical--medium'>{identifierAttributes.name}</h2>,
             <Identifier key={identifierAttributes.name} attributes={identifierAttributes} changeIdentifierValue={changeIdentifierValue} changeIdentifierDataType={changeIdentifierDataType} changeIdentifierOptions={changeIdentifierOptions}/>
-          </Row>
+          ]
         )}
-      </Row>
+      </Grid>
     )
   }
 }
