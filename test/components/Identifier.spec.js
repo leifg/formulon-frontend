730038653f56b530806d60da37c1fb0415d51e8d
Leif Gensert <leif@leif.io>
2016-04-17 21:39:22 +0200

    refactor: fix specs
    remove IdentifierValue, IdentifierOptions and IdentifierDataType from
    tests

--- test/components/Identifier.spec.js
+++ test/components/Identifier.spec.js
@@ -14,84 +14,16 @@ describe('Identifier', () => {
   jsdom()
 
   const attributes = {
-    dataType: 'number'
+    dataType: 'number',
+    options: {
+      length: 8,
+      scale: 0,
+    }
   }
 
-  it('is a FieldSet', () => {
+  it('is a Row', () => {
     renderer.render(<Identifier attributes={attributes}/>)
     const identifier = renderer.getRenderOutput()
-    expect(identifier.type.name).to.eq('FieldSet')
-  })
-
-  it('contains 1 Row', () => {
-    renderer.render(<Identifier attributes={attributes}/>)
-    const identifier = renderer.getRenderOutput()
-    const children = identifier.props.children
-    expect(children.type.name).to.eq('Row')
-  })
-})
-
-describe('IdentifierValue', () => {
-  const name = 'lg__testName__c'
-  const renderer = TestUtils.createRenderer()
-
-  context('number', () => {
-    const dataType = 'number'
-    const value = 'content'
-    const renderer = TestUtils.createRenderer()
-
-    beforeEach(() => {
-      renderer.render(<IdentifierValue name={name} value={value} dataType={dataType}/>)
-    })
-
-    it('is a FieldSet', () => {
-      const identifierName = renderer.getRenderOutput()
-      expect(identifierName.type.name).to.eq('FieldSet')
-    })
-  })
-
-  context('text', () => {
-    const dataType = 'text'
-    const value = 'content'
-    const renderer = TestUtils.createRenderer()
-
-    beforeEach(() => {
-      renderer.render(<IdentifierValue name={name} value={value} dataType={dataType}/>)
-    })
-
-    it('is a FieldSet', () => {
-      const identifierName = renderer.getRenderOutput()
-      expect(identifierName.type.name).to.eq('FieldSet')
-    })
-  })
-
-  context('checkbox', () => {
-    let dataType = 'checkbox'
-    const value = 'true'
-    const renderer = TestUtils.createRenderer()
-
-    beforeEach(() => {
-      renderer.render(<IdentifierValue name={name} value={value} dataType={dataType}/>)
-    })
-
-    it('is a FieldSet', () => {
-      const identifierName = renderer.getRenderOutput()
-      expect(identifierName.type.name).to.eq('FieldSet')
-    })
-  })
-})
-
-describe('IdentifierDataType', () => {
-  const name = 'lg__testName__c'
-  const value = 'text'
-  const renderer = TestUtils.createRenderer()
-
-  beforeEach(() => {
-    renderer.render(<IdentifierDataType name={name} value={value} />)
-  })
-
-  it('is a FieldSet', () => {
-    const identifierDataType = renderer.getRenderOutput()
-    expect(identifierDataType.type.name).to.eq('FieldSet')
+    expect(identifier.type.name).to.eq('Row')
   })
 })
