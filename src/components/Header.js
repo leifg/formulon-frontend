45c8270d170d19aca560a064314d0f79781d4a63
Leif Gensert <leif@leif.io>
2020-03-21 19:13:43 -0700

    chore: design work

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,31 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div class="slds-builder-header_container">
-      <header class="slds-builder-header">
-        <div class="slds-builder-header__item">
-          <div class="slds-builder-header__item-label slds-media slds-media_center">
-            <div class="slds-media__figure">
-              <span class="slds-icon_container slds-icon-utility-builder slds-current-color">
-              <svg class="slds-icon slds-icon_x-small" aria-hidden="true">
+    <div className="slds-builder-header_container">
+      <header className="slds-builder-header">
+        <div className="slds-builder-header__item">
+          <div className="slds-builder-header__item-label slds-media slds-media_center">
+            <div className="slds-media__figure">
+              <span className="slds-icon_container slds-icon-utility-builder slds-current-color">
+              <svg className="slds-icon slds-icon_x-small" aria-hidden="true">
                   <use href='/assets/icons/utility-sprite/svg/symbols.svg#builder' />
                 </svg>
               </span>
             </div>
-            <div class="slds-media__body">Formulon</div>
+            <div className="slds-media__body">Formulon</div>
           </div>
         </div>
-        <div class="slds-builder-header__item slds-builder-header__utilities">
-          <div class="slds-builder-header__utilities-item">
-            <a href="mailto:formulon@leif.io" class="slds-builder-header__item-action slds-media slds-media_center">
-              <div class="slds-media__figure">
-                <span class="slds-icon_container slds-icon-utility-help slds-current-color">
-                  <svg class="slds-icon slds-icon_x-small" aria-hidden="true">
+        <div className="slds-builder-header__item slds-builder-header__utilities">
+          <div className="slds-builder-header__utilities-item">
+            <a href="mailto:formulon@leif.io" className="slds-builder-header__item-action slds-media slds-media_center">
+              <div className="slds-media__figure">
+                <span className="slds-icon_container slds-icon-utility-help slds-current-color">
+                  <svg className="slds-icon slds-icon_x-small" aria-hidden="true">
                     <use href="/assets/icons/utility-sprite/svg/symbols.svg#help" />
                   </svg>
                 </span>
               </div>
-              <div class="slds-media__body">Feedback</div>
+              <div className="slds-media__body">Feedback</div>
             </a>
           </div>
         </div>
